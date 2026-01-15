caee6b2b8e90674a8f9f5a01bddcbe0945d7fda9
David Craig Cothren <dacracot@yahoo.com>
2026-01-15 10:38:59 -0800

    event choice

--- event.js
+++ event.js
@@ -37,26 +37,15 @@ notBingo = "bingox.png";
 hasBingo = "bingo.png";
 //======================================================================
 function chooseEvent(select) {
-
-console.log("-1- selected:"+select.options[select.selectedIndex].value);
-
 	if(select.options[select.selectedIndex].value == "none") {
-
-console.log("-2- selected:"+select.options[select.selectedIndex].value);
-
 		alert("Select an event from the list.");
-
-console.log("-3- selected:"+select.options[select.selectedIndex].value);
-
 		}
 	else {
 
-console.log("-4- selected:"+select.options[select.selectedIndex].value);
+console.log("selected:"+select.options[select.selectedIndex].value);
 
-		document.getElementById("chooseEvent").style.display = "none";
-
-console.log("-5- selected:"+select.options[select.selectedIndex].value);
-
+		document.getElementById("before").style.display = "none";
+		document.getElementById("during").style.display = "";
 		}
 	}
 //======================================================================
